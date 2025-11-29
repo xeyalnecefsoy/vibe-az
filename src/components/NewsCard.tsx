f3d2cbf86d955f6476b501f21b6dd77bf008fc30
xeyalnecefsoy <xeyalnecefsoy@gmail.com>
2025-11-29 15:32:47 +0400

    Add unoptimized prop to NewsCard images

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -21,6 +21,7 @@ export default function NewsCard({ item }: { item: NewsItem }) {
           fill
           className="object-cover transition-transform duration-300 group-hover:scale-105"
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          unoptimized
           suppressHydrationWarning
         />
         {item.tag ? (
